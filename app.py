76d2e8776f5fb5729f7eada9fb26af30f7567862
Davidhaba <154765655+Davidhaba@users.noreply.github.com>
2025-11-30 19:46:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
+import os
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -72,5 +73,6 @@ def handle_typing(data):
     username = data['username']
     emit('display_typing', {'username': username}, room=room, broadcast=True)
 
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    socketio.run(app, host="0.0.0.0", port=port)
