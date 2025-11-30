3a2c3b5d21a0c4716c112dd8fbf8befb778e747d
Davidhaba <154765655+Davidhaba@users.noreply.github.com>
2025-11-30 23:01:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -74,5 +74,5 @@ def handle_typing(data):
     emit('display_typing', {'username': username}, room=room, broadcast=True)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=8080)
+
